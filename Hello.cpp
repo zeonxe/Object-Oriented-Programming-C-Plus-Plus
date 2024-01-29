cb81cd1db9228d95946df6ada5878e0dae355434
Ziad Sadek <008241851@coyote.csusb.edu>
2024-01-29 15:35:17 -0800

    committed Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,12 @@
+/****
+ *  {Hello cpp lab}
+ ***/
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << end1;
+    printf("Hello World again!\n");
+    return 0;
+}
