de5cf567a4f3510411b129687d6043080f1b553a
zeonxe <157756060+zeonxe@users.noreply.github.com>
2024-02-13 16:36:42 -0800

    Add files via upload

---
+++ Lab3/Lab3ifelse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+int main()
+
+{
+    int x = 4;
+    int y = 5;
+
+    if(x>y) {
+        printf("x is greater than y\n");
+    }else{
+        printf("x is less than or equal to y\n");
+    }
+
+    return 0;
+
+
+}
---
+++ Lab3/Lab3while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main()
+{
+    int x = 0;
+    int y = 5;
+
+    while(x<y) {
+        printf("%i\n",x);
+        x++; // x = x + 1; 
+    
+    }
+    
+
+}
